9bf7751950117eb702e54ffaa068894f1dc99329
Đăng Vĩnh <tranhgio15@gmail.com>
2026-03-30 19:17:52 +0700

    Update ngoc-diep timestamp to 19:17

--- app/data/goldShops.ts
+++ app/data/goldShops.ts
@@ -12,7 +12,7 @@ export const GOLD_SHOPS: GoldShop[] = [
     name: "Ngọc Diệp",
     location: "Đà Nẵng",
     image: "images/ngoc-diep.jpg",
-    updatedAt: "16:17",
+    updatedAt: "19:17",
   },
   {
     id: "ngoc-thinh",
