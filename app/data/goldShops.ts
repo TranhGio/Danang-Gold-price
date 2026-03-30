350ab0448e75a134789a8ef59f5fda717ae0035c
Đăng Vĩnh <tranhgio15@gmail.com>
2026-03-30 14:40:19 +0700

    Update ngoc-thinh timestamp to 14:40

--- app/data/goldShops.ts
+++ app/data/goldShops.ts
@@ -19,7 +19,7 @@ export const GOLD_SHOPS: GoldShop[] = [
     name: "Ngọc Thịnh",
     location: "Đà Nẵng",
     image: "images/ngoc-thinh.jpg",
-    updatedAt: "13:39",
+    updatedAt: "14:40",
   },
   {
     id: "kim-khanh-viet-hung",
