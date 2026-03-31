33dad1aaab56ac986846cdac0a22f9f605e4c04e
Đăng Vĩnh <tranhgio15@gmail.com>
2026-03-31 13:52:08 +0700

    Update popup redirect URL

--- app/data/popup.ts
+++ app/data/popup.ts
@@ -1,4 +1,4 @@
 export const POPUP_CONFIG = {
   message: "Cảm ơn bạn đã quan tâm đến trang web giá vàng Đà Nẵng của chúng tôi, cập nhật giá vàng của các tiệm vàng tại Đà Nẵng mỗi ngày theo giờ cho mọi người tiện theo dõi.",
-  redirectUrl: "http://giavangdanang.com",
+  redirectUrl: "https://giavangdanang.com/",
 };
