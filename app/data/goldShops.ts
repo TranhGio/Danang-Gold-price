8a8988bf5121fe87a35d06adf22421d24b5ee512
Đăng Vĩnh <tranhgio15@gmail.com>
2026-03-31 10:44:08 +0700

    Update kim-khanh-viet-hung price image (10:44)

--- app/data/goldShops.ts
+++ app/data/goldShops.ts
@@ -26,7 +26,7 @@ export const GOLD_SHOPS: GoldShop[] = [
     name: "Kim Khánh Việt Hùng",
     location: "Đà Nẵng",
     image: "images/kim-khanh-viet-hung.jpg",
-    updatedAt: "09:56",
+    updatedAt: "10:44",
   },
   {
     id: "tru-cam-le",
