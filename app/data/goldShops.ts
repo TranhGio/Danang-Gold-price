25fd9e33362baf3997484e889a3defeb7678992a
Đăng Vĩnh <tranhgio15@gmail.com>
2026-04-08 10:03:28 +0900

    Update tru-cam-le price image (08:03)

--- app/data/goldShops.ts
+++ app/data/goldShops.ts
@@ -33,7 +33,7 @@ export const GOLD_SHOPS: GoldShop[] = [
     name: "Trữ Cẩm Lệ",
     location: "Đà Nẵng",
     image: "images/tru-cam-le.jpg",
-    updatedAt: "09:50",
+    updatedAt: "08:03",
   },
 ];
 
