87e694e942c09fed183d081797aba3bf0210c2f9
Đăng Vĩnh <tranhgio15@gmail.com>
2026-04-08 18:09:28 +0900

    Update ngoc-diep price image (16:09)

--- app/data/goldShops.ts
+++ app/data/goldShops.ts
@@ -12,7 +12,7 @@ export const GOLD_SHOPS: GoldShop[] = [
     name: "Ngọc Diệp",
     location: "Đà Nẵng",
     image: "images/ngoc-diep.jpg",
-    updatedAt: "13:35",
+    updatedAt: "16:09",
   },
   {
     id: "ngoc-thinh",
